0492790e8668da583dcbdca8473dd09e35de9e1c
catalina <catalinaruedaf@hotmail.com>
2026-07-19 16:37:32 +1000

    workign on the desktop styles

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,14 +6,23 @@ import Booking from "./components/Booking";
 
 export default function Home() {
   return (
-    <section className="home container">
-      <h1 className="title__home text-present-1">Design solutions made easy</h1>
-      <p className="description__home text-present-5">With over ten llllyear of experience in various deisng disciplines. I'm your one -stop shop for your design needs.</p>
-      <Features />
-        <Information />
-        <Work/>
-        <Booking />
+    <section className="home">
+      <div className="container hero">
+        <h1 className="title__home text-present-1">
+          Design solutions made easy
+        </h1>
 
+        <p className="description__home text-present-5">
+          With over ten years of experience in various design disciplines,
+          I'm your one-stop shop for your design needs.
+        </p>
+      </div>
+        <div className="home-content">
+          <Features />
+          <Information />
+          <Work/>
+          <Booking />
+       </div>
     </section>
   );
 }
